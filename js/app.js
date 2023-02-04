fdb76be67793e4f4c3c78ab41849af412549598b
yanay21 <77297267+yanay21@users.noreply.github.com>
2023-02-04 20:07:36 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,74 +1,84 @@
-(function() {
+(function(){
     'use strict';
 
-    angular.module('NarrowItDownApp', [])
-        .controller('NarrowItDownController', NarrowItDownController)
-        .service('MenuSearchService', MenuSearchService)
-        .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
-        .directive('foundItems', FoundItems);
+    angular.module('NarrowItDownApp',[])
+    .controller('NarrowItDownController',NarrowItDownController)
+    .service('MenuSearchService', MenuSearchService)
+    .directive('foundItems',FoundItemsDirective)
+    .constant('ApiBasePath', 'https://coursera-jhu-default-rtdb.firebaseio.com/menu_items.json');
 
-    function FoundItems() {
+
+    function FoundItemsDirective(){
         var ddo = {
-            restrict: 'E',
             templateUrl: 'foundItems.html',
             scope: {
-                foundItems: '<',
-                onEmpty: '<',
+                items : '<',
                 onRemove: '&'
             },
             controller: NarrowItDownController,
-            controllerAs: 'menu',
+            controllerAs: 'narrow',
             bindToController: true
         };
-
         return ddo;
     }
 
     NarrowItDownController.$inject = ['MenuSearchService'];
+    function NarrowItDownController(MenuSearchService){
+        var narrowControl = this;
+        narrowControl.items2 = "this is what"
+        
 
-    function NarrowItDownController(MenuSearchService) {
-        var menu = this;
-        menu.shortName = '';
-
-        menu.matchedMenuItems = function(searchTerm) {
-            var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
-
-            promise.then(function(items) {
-                if (items && items.length > 0) {
-                    menu.message = '';
-                    menu.found = items;
-                } else {
-                    menu.message = 'Nothing found!';
-                    menu.found = [];
-                }
-            });
-        };
-
-        menu.removeMenuItem = function(itemIndex) {
-            menu.found.splice(itemIndex, 1);
+        narrowControl.getItems = function(searchterm){
+            var promise =  MenuSearchService.getMatchedMenuItems(searchterm);
+            promise.then(function (response) {
+                narrowControl.found = response;
+                // console.log("we found them ")
+              })
+              .catch(function (error) {
+                console.log(error);
+              })
         }
+
+        narrowControl.removeItem = function(index, elements){
+            narrowControl.found = MenuSearchService.removeItem(index, elements)
+        };
     }
+
 
     MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+    function MenuSearchService($http,ApiBasePath){
+        var menuServ = this;
 
-    function MenuSearchService($http, ApiBasePath) {
-        var service = this;
-
-        service.getMatchedMenuItems = function(searchTerm) {
+        menuServ.getMatchedMenuItems = function(searchTerm){
             return $http({
-                method: "GET",
-                url: (ApiBasePath + "/menu_items.json")
-            }).then(function(response) {
-                var foundItems = [];
-
-                for (var i = 0; i < response.data['menu_items'].length; i++) {
-                    if (searchTerm.length > 0 && response.data['menu_items'][i]['description'].toLowerCase().indexOf(searchTerm) !== -1) {
-                        foundItems.push(response.data['menu_items'][i]);
+                method: 'GET',
+                url: ApiBasePath,
+                params: {
+                    description: searchTerm
+                  }
+            }).then(function(result){
+                if (searchTerm == ""){
+                    return []
+                }else{
+                    var foundItems = []
+                    for (var item in result.data){
+                        var entireMenu = result.data[item]
+                        var itemsInMenu = entireMenu["menu_items"]
+                        itemsInMenu.forEach((menu) =>{
+                                if (menu['description'].includes(searchTerm)){
+                                    foundItems.push(menu);
+                                }
+                            })      
                     }
+                   
+                    return foundItems;
                 }
+            })
+        }
+        menuServ.removeItem= function(index,elements){
+            elements.splice(index,1);
+            return elements;
 
-                return foundItems;
-            });
         };
-    }
+    } 
 })();
